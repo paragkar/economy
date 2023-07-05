283591e3acc6aaff1b91a5252bc3895c24dbbf32
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-05 14:07:39 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -128,6 +128,8 @@ dfindex = dfindex.sort_values(dfindex.columns[-1], ascending = False)
 
 dfindex = dfindex.drop("General")
 
+st.write(dfindex.shift(1,axis=1))
+
 years = sorted(set([x.year for x in list(dfindex.columns)]))
 
 x_axis_title_dict = {"RuralIndex":"<b>Indian CPI Rural Index Trend<b>", "UrbanIndex":"<b>Indian CPI Urban Index Trend<b>", "CombIndex":
