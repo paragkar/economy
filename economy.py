aff40ab4cb9b0f4395338c2b8dee76966380d2a1
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-06 15:18:10 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -300,44 +300,48 @@ if selected_metric == "CPI":
 		texttemplate =""
 		tickvals = years
 
-	hovertextgen = []
 	genindex = dfindex.loc["General",:].reset_index().T
 	dfindex = dfindex.drop("General")
 	genindex.columns = list(genindex.loc["Date",:])
 	genindex=genindex.drop("Date")
-	datagen1 = data(genindex,"Rainbow",texttemplate, hovertextgen)
-	figgen1 = go.Figure(data=datagen1)
 
 
 	geninflation = dfinflation.loc["General",:].reset_index().T
 	dfinflation = dfinflation.drop("General")
 	geninflation.columns = list(geninflation.loc["Date",:])
 	geninflation=geninflation.drop("Date")
-	datagen2 = data(geninflation,"Rainbow",texttemplate, hovertextgen)
-	figgen2 = go.Figure(data=datagen2)
 
 
 	geninfweighted = dfinfweighted.loc["General",:].reset_index().T
 	dfinfweighted = dfinfweighted.drop("General")
 	geninfweighted.columns = list(geninfweighted.loc["Date",:])
 	geninfweighted=geninfweighted.drop("Date")
-	datagen3 = data(geninfweighted,"Rainbow",texttemplate, hovertextgen)
-	figgen3 = go.Figure(data=datagen3)
+
 
 	hovertext1 = htext_cpi_subcat(dfindex, dfinflation, dfinfweighted,1)
 	hovertext2 = htext_cpi_subcat(dfindex, dfinflation, dfinfweighted,2)
 	hovertext3 = htext_cpi_subcat(dfindex, dfinflation, dfinfweighted,3)
+	hovertextgen1 = htext_cpi_subcat(genindex, geninflation, geninfweighted,1)
+	hovertextgen2 = htext_cpi_subcat(genindex, geninflation, geninfweighted,2)
+	hovertextgen3 = htext_cpi_subcat(genindex, geninflation, geninfweighted,3)
 	hoverlabel_bgcolor = "#000000" #subdued black
 
 
 	data1 = data(dfindex,"Rainbow",texttemplate, hovertext1)
 	data2 = data(dfinflation,"Rainbow",texttemplate, hovertext2)
 	data3 = data(dfinfweighted,"Rainbow",texttemplate, hovertext3)
+	datagen1 = data(genindex,"Rainbow",texttemplate, hovertextgen1)
+	datagen2 = data(geninflation,"Rainbow",texttemplate, hovertextgen2)
+	datagen3 = data(geninfweighted,"Rainbow",texttemplate, hovertextgen3)
+
 
 
 	fig1 = go.Figure(data=data1)
 	fig2 = go.Figure(data=data2)
 	fig3 = go.Figure(data=data3)
+	figgen1 = go.Figure(data=datagen1)
+	figgen2 = go.Figure(data=datagen2)
+	figgen3 = go.Figure(data=datagen3)
 
 
 	figupdate(fig1, dfindex, dates, x_axis_title_dict1, selected_feature, 650, tickvals, hoverlabel_bgcolor)
