b42bd81ba419ec20c3584bed5a628a729b32f1a7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-07 14:28:04 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -487,7 +487,7 @@ if selected_metric == "GST India":
 	dfigst = dfigst[date_range_list]
 	dfcess = dfcess[date_range_list]
 
-	st.write(dfcgsts.replace(np.nan,0).round(1))
+	st.write(dfcgsts.replace(0.np.nan).round(1).dropna(axis=0, how="all"))
 
 
 
