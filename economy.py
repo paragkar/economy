a85d0f7a9878259b4d5a4be40d4c2641aafcae0c
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-07 13:54:12 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -458,6 +458,8 @@ if selected_metric == "GST India":
 
 	dfgst = dfgst.set_index("Date")
 
+	dfgst = dfgst.replace("-",0)
+
 	dfgst["Amount"] = dfgst["Amount"].astype("float")
 
 	dfcgsts = dfgst[dfgst["Type"]=="CGSTS"].drop(columns = "Type").reset_index().groupby(["Date","State"])["Amount"].sum()
