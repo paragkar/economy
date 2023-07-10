1e73ebf4fc9c0ab211d8b57bd3be80b7a13c18e0
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-10 12:00:26 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -656,11 +656,11 @@ if selected_metric == "GST India":
 
 	#calculating their percent share of total
 
-	dfcgstsprec = round((dfcgsts/dfcgststotal.values)*100,1).sort_index(ascending= False)
-	dfsgstprec = round((dfsgst/dfsgsttotal.values)*100,1).sort_index(ascending= False)
-	dfigstprec = round((dfigst/dfigsttotal.values)*100,1).sort_index(ascending= False)
-	dfcessprec = round((dfcess/dfcesstotal.values)*100,1).sort_index(ascending= False)
-	dfgstallprec = round((dfgstall/dfgstalltotal.values)*100,1).sort_index(ascending= False)
+	dfcgstsprec = round((dfcgsts/dfcgststotal.values)*100,1)
+	dfsgstprec = round((dfsgst/dfsgsttotal.values)*100,1)
+	dfigstprec = round((dfigst/dfigsttotal.values)*100,1)
+	dfcessprec = round((dfcess/dfcesstotal.values)*100,1)
+	dfgstallprec = round((dfgstall/dfgstalltotal.values)*100,1)
 
 
 	#calculating data for individual figures of heatmaps
