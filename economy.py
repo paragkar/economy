2b8183187a871a8f2a74f2f738f27f3a73059aa7
Parag Kar <40283772+paragkar@users.noreply.github.com>
2023-07-13 09:27:17 +0530

    Update economy.py

--- economy.py
+++ economy.py
@@ -100,7 +100,7 @@ def data(df,colorscale,texttemplate, hovertext):
 		colorscale=colorscale,
 		showscale=False,
 		texttemplate=texttemplate,
-		textfont={"size":10},
+		textfont={"size":12},
 		# reversescale=True,
 						),
 			]
